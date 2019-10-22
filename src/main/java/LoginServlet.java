9403f884fe03bffbb80ec62a3cee9140a313074a
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-22 15:34:46 -0500

    had ti change my loginservlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,19 +1,30 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-    @WebServlet(name = "LoginServlet", urlPatterns = "/login")
+    @WebServlet("/login")
     public class LoginServlet extends HttpServlet {
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             if (request.getMethod().equalsIgnoreCase("post")) {
                 String username = request.getParameter("username");
                 String password = request.getParameter("password");
                 if (username.equals("admin") && password.equals("password")) {
                     response.sendRedirect("/profile");
+                }else{
+                    response.sendRedirect("/login");
                 }
             }
+//        request.setAttribute("password", password);
+//        request.setAttribute("username", username);
+//        request.getRequestDispatcher("/login.jsp").forward(request, response);
         }
 
     }
