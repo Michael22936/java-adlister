7fc1c74ab26b5692905b4ca9711f0968f2d92712
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-24 16:27:34 -0500

    complete jdbc-exercise

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,53 @@
+import com.mysql.cj.jdbc.Driver;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config){
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword());
+
+        }catch (SQLException e){
+            e.getErrorCode();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ads");
+            while (resultSet.next()){
+                Ad ad = new Ad(resultSet.getLong("id"),
+                        resultSet.getLong("user_id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("description"));
+                ads.add(ad);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = String.format("INSERT into ads (title, description) values('%s', '%s')", ad.getTitle(), ad.getDescription());
+            statement.execute(sql);
+        }catch (SQLException e){
+            e.getStackTrace();
+        }
+        return 0L;
+    }
+}
