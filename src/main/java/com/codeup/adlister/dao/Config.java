cf4170543006ee34c0c1f6ed3c02c8648ef2cbd5
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-25 16:29:31 -0500

    complete user-input-exercise

--- src/main/java/com/codeup/adlister/dao/Config.java
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -1,13 +1,4 @@
 package com.codeup.adlister.dao;
 
 class Config {
-    public String getUrl() {
-        return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
-    }
-    public String getUsername() {
-        return "bob";
-    }
-    public String getPassword() {
-        return "password";
-    }
 }
